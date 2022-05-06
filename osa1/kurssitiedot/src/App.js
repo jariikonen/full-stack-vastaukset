f8726e7d195a87dcecd3643c0195dcfba1e0457d
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2022-05-06 10:40:48 +0300

    teht. 1.1, valmis

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,3 +1,21 @@
+const Header = (props) => (
+  <div>
+    <h1>{props.course}</h1>
+  </div>
+)
+
+const Content = (props) => (
+  <>
+    <p>{props.parts[0]} {props.exerciseCounts[0]}</p>
+    <p>{props.parts[1]} {props.exerciseCounts[1]}</p>
+    <p>{props.parts[2]} {props.exerciseCounts[2]}</p>
+  </>
+)
+
+const Total = (props) => (
+  <p>Number of exercises {props.exercises}</p>
+)
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -6,20 +24,14 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  const parts = [part1, part2, part3]
+  const exerciseCounts = [exercises1, exercises2, exercises3]
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={parts} exerciseCounts={exerciseCounts} />
+      <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
