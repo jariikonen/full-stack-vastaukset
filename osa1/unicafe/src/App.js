a5b1330b3ad38a1fc62cbe009e8969b803e64d90
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2022-07-17 07:49:44 +0300

    teht. 1.11

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -4,7 +4,7 @@ const Button = ({ handleClick, text }) =>
   <button onClick={handleClick}>{text}</button>
 
 const StatisticLine = ({ text, value }) =>
-  <p>{text} {value}</p>
+  <tbody><tr><td>{text}</td><td>{value}</td></tr></tbody>
 
 const Statistics = ({ good, neutral, bad }) => {
   const all=good+neutral+bad
@@ -13,14 +13,14 @@ const Statistics = ({ good, neutral, bad }) => {
 
   if ( all > 0 ) {
     return (
-      <>
+      <table>
         <StatisticLine text='good' value={good} />
         <StatisticLine text='neutral' value={neutral} />
         <StatisticLine text='bad' value={bad} />
         <StatisticLine text='all' value={all} />
         <StatisticLine text='average' value={average.toFixed(2)} />
         <StatisticLine text='positive' value={(positive*100).toFixed(1)+'%'} />
-      </>
+      </table>
     )
   }
 
