308acd813bcd3d7d9a999af4ce81ea5757640ffe
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2022-07-18 17:03:24 +0300

    teht. 2.5

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,36 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Total = ({ parts }) => {
-  const total = parts.reduce(
-    (sum, part) =>
-      sum + part.exercises,
-    0
-  )
-
-  return (
-    <p><b>total of {total} excercises</b></p>
-  )
-}
-
-const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) =>
-  <>
-    {parts.map(part =>
-      <Part key={part.id} part={part} />
-    )}
-  </>
-
-const Course = ({ course }) => (
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </div>
-)
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
