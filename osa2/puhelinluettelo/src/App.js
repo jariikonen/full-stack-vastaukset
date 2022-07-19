665ecefaff9ebd44f12f69fd0837843ee75a84f6
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2022-07-19 10:33:31 +0300

    teht. 2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,10 +2,15 @@ import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [personsToShow, setPersonsToShow] = useState(persons)
+  const [filterValue, setFilterValue] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -24,6 +29,17 @@ const App = () => {
       };
       console.log('added new person:', personObject);
       setPersons(persons.concat(personObject));
+
+      const show = personObject.name.toLowerCase().includes(
+        filterValue.toLowerCase()
+      );
+      if (show) {
+        console.log('new person is shown');
+        setPersonsToShow(personsToShow.concat(personObject));
+      }
+      else {
+        console.log('new person is not shown');
+      };
     };
 
     setNewName('');
@@ -35,10 +51,27 @@ const App = () => {
 
   const handleNumberChange = (event) =>
     setNewNumber(event.target.value);
+  
+  const handleFilterChange = (event) => {
+    setFilterValue(event.target.value)
+    const found = persons.filter(person =>
+      person.name.toLowerCase().includes(
+        event.target.value.toLowerCase()
+    ));
+    setPersonsToShow(found);
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with: <input
+            onChange={handleFilterChange}
+          />
+        </div>
+      </form>
+      <h2>Add new contact</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input
@@ -57,12 +90,12 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>
+      {personsToShow.map(person =>
         <div key={person.name}>{person.name} {person.number}</div>
       )}
     </div>
   );
 
-}
+};
 
 export default App;
