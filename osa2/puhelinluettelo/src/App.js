de11e682835271777962c8c645dcb28b74f2e0b8
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2022-07-19 14:18:42 +0300

    teht. 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Filter = ({ filterHandler }) =>
   <form>
@@ -39,16 +40,23 @@ const Persons = ({ persons }) =>
   </>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [personsToShow, setPersonsToShow] = useState(persons)
   const [filterValue, setFilterValue] = useState('')
+
+  useEffect (() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled');
+        setPersons(response.data);
+        setPersonsToShow(persons)
+      });
+  }, []);
+  console.log('render', persons.length, 'persons:', persons);
 
   const addPerson = (event) => {
     event.preventDefault();
