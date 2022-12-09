a20338a46cb9e2d421da09a03c51c342fc120b58
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2022-12-09 06:14:17 +0200

    teht. 2.19 ja 2.20

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -10,8 +10,11 @@ const Filter = ({ filterHandler }) => (
 )
 
 const PersonForm = ({
-    submitHandler, newNameState, nameChangeHandler,
-    newNumberState, numberChangeHandler
+    submitHandler,
+    newNameState,
+    nameChangeHandler,
+    newNumberState,
+    numberChangeHandler
 }) => (
   <form onSubmit={submitHandler}>
     <div>
@@ -38,12 +41,26 @@ const Person = ({ name, number, handleDelete }) => (
   </div>
 )
 
+const Notification = ({ message, type }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className={type}>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [personsToShow, setPersonsToShow] = useState([])
   const [filterValue, setFilterValue] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState(null)
+  const [notificationType, setNotificationType] = useState(null)
 
   useEffect (() => {
     personService
@@ -71,17 +88,36 @@ const App = () => {
     }
   }
 
+  const setNotification = (message, type) => {
+    setNotificationMessage(message)
+    setNotificationType(type)
+    setTimeout(() => {
+      setNotificationMessage(null)
+      setNotificationType(null)
+    }, 5000)
+  }
+
+  const clearLists = (id) => {
+    setPersons(persons.filter(n => n.id !== id))
+    setPersonsToShow(personsToShow.filter(n => n.id !== id))
+  }
+
   const createPerson = (newPerson) => {
     personService
         .createPerson(newPerson)
         .then(returnedPerson => {
           setPersons(persons.concat(returnedPerson))
-          console.log('added new person:', returnedPerson)
           showIfVisible(returnedPerson)
           setNewName('')
           setNewNumber('')
+          setNotification(
+            `Added ${returnedPerson.name} to the phonebook`,
+            'success'
+          )
+          console.log('added new person:', returnedPerson)
         })
         .catch(error => {
+          setNotification(error.message, 'error')
           console.log(error)
         })
   }
@@ -96,11 +132,25 @@ const App = () => {
           setPersonsToShow(persons.map(person =>
             person.id !== newPerson.id ? person : newPerson
           ))
-          console.log('updated person:', returnedPerson)
           setNewName('')
           setNewNumber('')
+          setNotification(
+            `Updated number for ${returnedPerson.name}`,
+            'success'
+          )
+          console.log('updated person:', returnedPerson)
         })
         .catch(error => {
+          if (error.code === 'ERR_BAD_REQUEST') {
+            setNotification(
+              `Update failed, ${newPerson.name} has been removed from the ` +
+                'server',
+              'error')
+            clearLists(newPerson.id)
+          }
+          else {
+            setNotification(error.message, 'error')
+          }
           console.log(error)
         })
   }
@@ -114,7 +164,7 @@ const App = () => {
     if (found) {
       if (window.confirm(
         `${newName} is already in the phonebook, ` +
-        'replace the old number with a new one?'
+          'replace the old number with a new one?'
       )) {
         updatePerson({
           ...found,
@@ -130,11 +180,13 @@ const App = () => {
     }
   }
 
-  const handleNameChange = (event) =>
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
 
-  const handleNumberChange = (event) =>
+  const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
   
   const handleFilterChange = (event) => {
     setFilterValue(event.target.value)
@@ -153,10 +205,23 @@ const App = () => {
       personService
         .deletePerson(id)
         .then(returnedId => {
-          setPersons(persons.filter(n => n.id !== id))
-          setPersonsToShow(personsToShow.filter(n => n.id !== id))
+          clearLists(returnedId)
+          setNotification(
+            `Removed ${person.name} from the phonebook`,
+            'success'
+          )
         })
         .catch(error => {
+          if (error.code === 'ERR_BAD_REQUEST') {
+            setNotification(
+              `Delete failed, ${person.name} has already been removed from ` +
+                'the server',
+              'error')
+            clearLists(id)
+          }
+          else {
+            setNotification(error.message, 'error')
+          }
           console.log(error)
         })
     }
@@ -165,6 +230,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification
+        message={notificationMessage}
+        type={notificationType}
+      />
       <Filter
         filterHandler={handleFilterChange}
       />
@@ -187,8 +256,8 @@ const App = () => {
               number={person.number}
               handleDelete={() => handleDeleteOf(person.id)}
             />
-          )}
-        )}
+          )
+        })}
     </div>
   )
 
