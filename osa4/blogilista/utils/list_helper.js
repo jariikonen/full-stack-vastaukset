46cf690f6aefbae567ceb64ed7527aa50741cb0d
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-01-09 19:03:40 +0200

    teht. 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,7 +6,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return undefined;
+  }
+
+  const { title, author, likes } = blogs.reduce(
+    (favorite, blog) => (blog.likes > favorite.likes ? blog : favorite),
+  );
+  return { title, author, likes };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
