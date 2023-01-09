f05ec76a79e429a9728c3c9f65b5b5a2d1968106
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-01-09 16:13:09 +0200

    teht. 4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const blogsRouter = require('./controllers/blogs');
+const {
+  unknownEndpoint,
+  errorHandler,
+} = require('./utils/middleware');
+
+const app = express();
+
+mongoose.set('strictQuery', false);
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose.connect(config.MONGODB_URI)
+  .then((result) => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+morgan.token('data', (request, response) => {
+  if (Object.values(request.body).length > 0) {
+    return JSON.stringify(request.body);
+  }
+  return ' ';
+});
+
+app.use(morgan(
+  ':method :url :status :res[content-length] - :response-time ms :data',
+));
+
+app.use('/api/blogs', blogsRouter);
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+module.exports = app;
