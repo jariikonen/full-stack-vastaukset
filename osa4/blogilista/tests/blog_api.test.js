78334a6c2e4642c7438f057a7330c33aa044fc78
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-01-10 18:23:17 +0200

    teht. 4.8 ja 4.9

---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const Blog = require('../models/blog');
+const helper = require('./test_helper');
+
+const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs);
+});
+
+test('right amount of blogs are returned as json', async () => {
+  const response = await api.get('/api/blogs');
+
+  expect(response.status).toBe(200);
+  expect(response.type).toBe('application/json');
+  expect(response.body).toHaveLength(2);
+});
+
+test('name of the id field is "id"', async () => {
+  const response = await api.get('/api/blogs');
+
+  expect(response.body[0].id).toBeDefined();
+  // eslint-disable-next-line no-underscore-dangle
+  expect(response.body[0]._id).not.toBeDefined();
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
--- osa4/blogilista/utils/config.js
+++ osa4/blogilista/utils/config.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
+const logger = require('./logger');
 
-const { MONGODB_URI, PORT } = process.env;
+const { NODE_ENV, PORT } = process.env;
+
+logger.info(`running in ${NODE_ENV} environment`);
+
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI;
 
 module.exports = {
   MONGODB_URI,
   PORT,
+  NODE_ENV,
 };
