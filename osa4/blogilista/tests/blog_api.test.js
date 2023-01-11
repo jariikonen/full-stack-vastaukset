e8f336edfec907702dac9243d96afe45693f1892
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-01-11 13:44:58 +0200

    refaktoroi blog_api-testit asianmukaisiin describe-lohkoihin

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -6,67 +6,73 @@ const helper = require('./test_helper');
 
 const api = supertest(app);
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
-  await Blog.insertMany(helper.initialBlogs);
-});
+describe('when there are initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+    await Blog.insertMany(helper.initialBlogs);
+  });
 
-test('right amount of blogs are returned as json', async () => {
-  const response = await api.get('/api/blogs');
+  test('right amount of blogs are returned as json', async () => {
+    const response = await api.get('/api/blogs');
 
-  expect(response.status).toBe(200);
-  expect(response.type).toBe('application/json');
-  expect(response.body).toHaveLength(2);
-});
+    expect(response.status).toBe(200);
+    expect(response.type).toBe('application/json');
+    expect(response.body).toHaveLength(2);
+  });
 
-test('name of the id field is "id" (not "_id")', async () => {
-  const response = await api.get('/api/blogs');
+  test('name of the id field is "id" (not "_id")', async () => {
+    const response = await api.get('/api/blogs');
 
-  expect(response.body[0].id).toBeDefined();
-  // eslint-disable-next-line no-underscore-dangle
-  expect(response.body[0]._id).not.toBeDefined();
-});
+    expect(response.body[0].id).toBeDefined();
+    // eslint-disable-next-line no-underscore-dangle
+    expect(response.body[0]._id).not.toBeDefined();
+  });
 
-test('a valid blog can be added', async () => {
-  await api
-    .post('/api/blogs')
-    .send(helper.blogArray[2])
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+  describe('adding a blog', () => {
+    test('a valid blog can be added', async () => {
+      await api
+        .post('/api/blogs')
+        .send(helper.blogArray[2])
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+      const blogsAtEnd = await helper.blogsInDb();
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
 
-  const blogTitles = blogsAtEnd.map((b) => b.title);
-  expect(blogTitles).toContain('Canonical string reduction');
-});
+      const blogTitles = blogsAtEnd.map((b) => b.title);
+      expect(blogTitles).toContain('Canonical string reduction');
+    });
 
-test('when "likes" field is not given a value, it receives value of 0', async () => {
-  await api
-    .post('/api/blogs')
-    .send(helper.blogWithoutLikesField)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+    test('when "likes" field is not given a value, it receives value of 0', async () => {
+      await api
+        .post('/api/blogs')
+        .send(helper.blogWithoutLikesField)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  const newBlogFromDb = blogsAtEnd.filter((b) => b.title === helper.blogWithoutLikesField.title)[0];
-  expect(newBlogFromDb.likes).toEqual(0);
-});
+      const blogsAtEnd = await helper.blogsInDb();
+      const newBlogFromDb = blogsAtEnd.filter(
+        (b) => b.title === helper.blogWithoutLikesField.title,
+      )[0];
+      expect(newBlogFromDb.likes).toEqual(0);
+    });
 
-test('posted blog without a title field returns status "400 Bad Request"', async () => {
-  await api
-    .post('/api/blogs')
-    .send(helper.blogWithoutTitleField)
-    .expect(400)
-    .expect('Content-Type', /application\/json/);
-});
+    test('posting a blog without a title fails with statuscode 400', async () => {
+      await api
+        .post('/api/blogs')
+        .send(helper.blogWithoutTitleField)
+        .expect(400)
+        .expect('Content-Type', /application\/json/);
+    });
 
-test('posted blog without a url field returns status "400 Bad Request"', async () => {
-  await api
-    .post('/api/blogs')
-    .send(helper.blogWithoutUrlField)
-    .expect(400)
-    .expect('Content-Type', /application\/json/);
+    test('posting a blog without a url fails with statuscode 400', async () => {
+      await api
+        .post('/api/blogs')
+        .send(helper.blogWithoutUrlField)
+        .expect(400)
+        .expect('Content-Type', /application\/json/);
+    });
+  });
 });
 
 afterAll(() => {
