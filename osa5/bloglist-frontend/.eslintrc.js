cdd2986658ea4f7ecde828cfe2359629c813a507
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-06-01 16:12:44 +0300

    korjaus aiemmin eslint --init -komennolla tehtyyn eslint:in asennukseen, joka poistaa testitiedostoihin liittyvät linttausongelmat
    Korvaa aiemmin eslint --init -komennolla tehdyn eslint-asennuksen kurssin ohjeissa annetulla esimerkkikonfiguraatiolla. Korvasin aiemmat package.json:in ja package-lock.json:in teht. 5.1 mainitusta github-repositoriosta löytyvillä tiedostoilla ja asensin eslint:in uudelleen kurssin ohjeiden mukaan. Olin asentanut eslint:in heti alussa eslint --init -komennolla ja airbnb-konfiguraatiolla, ennen kuin huomaisin tästä varoitettavan. Muutos korjaa eslint:in ongelmat testitiedostoissa, mutta eslint-konfiguraatio yksinkertaistui huomattavasti.

--- osa5/bloglist-frontend/.eslintrc.js
+++ osa5/bloglist-frontend/.eslintrc.js
@@ -1,26 +1,56 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+  'env': {
+    'browser': true,
+    'es6': true,
+    'jest/globals': true,
   },
-  extends: [
+  'extends': [
+    'eslint:recommended',
     'plugin:react/recommended',
-    'airbnb',
   ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
   },
-  plugins: [
-    'react',
+  'plugins': [
+    'react', 'jest',
   ],
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/function-component-definition': ['error', {
-      namedComponents: 'arrow-function',
-      unnamedComponents: 'function-expression',
-    }],
+  'rules': {
+    'indent': [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'always',
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off',
+  },
+  'settings': {
+    'react': {
+      'version': 'detect',
+    },
   },
 };
