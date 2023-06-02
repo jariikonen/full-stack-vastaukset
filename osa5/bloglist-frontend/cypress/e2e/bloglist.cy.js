bb1c3a6823ebbb96983300a06187eb495129da5b
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-06-02 14:08:17 +0300

    teht. 5.18

--- osa5/bloglist-frontend/cypress/e2e/bloglist.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/bloglist.cy.js
@@ -1,12 +1,36 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
-    cy.visit('http://localhost:3000');
+    cy.request('POST', 'http://localhost:3003/api/users', {
+      username: 'jikon',
+      name: 'Jari Ikonen',
+      password: 'salainen',
+    }).then(() => {
+      cy.visit('http://localhost:3000');
+    });
   });
 
   it('Login form is shown', function() {
     cy.get('#username');
     cy.get('#password');
     cy.get('#login-button');
+  });
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('jikon');
+      cy.get('#password').type('salainen');
+      cy.get('#login-button').click();
+
+      cy.contains('logged in as Jari Ikonen');
+    });
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('tuntematon');
+      cy.get('#password').type('ei toimi');
+      cy.get('#login-button').click();
+
+      cy.contains('wrong username or password');
+    });
   });
 });
