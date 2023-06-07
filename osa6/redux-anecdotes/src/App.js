1f6c949e09531c9aef725e5ed3fccdb98d6e03ef
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-06-07 12:35:51 +0300

    teht. 6.14 ja 6.15

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,9 +1,18 @@
-import AnecdoteForm from './components/AnecdoteForm.js';
-import AnecdoteList from './components/AnecdoteList.js';
-import Filter from './components/Filter.js';
-import Notification from './components/Notification.js';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import AnecdoteForm from './components/AnecdoteForm';
+import AnecdoteList from './components/AnecdoteList';
+import Filter from './components/Filter';
+import Notification from './components/Notification';
+import anecdoteService from './services/anecdotes';
+import { setAnecdotes } from './reducers/anecdoteReducer';
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdoteService
+      .getAll().then((anecdotes) => dispatch(setAnecdotes(anecdotes)));
+  }, [dispatch]);
 
   return (
     <div>
