306874b5bff6d20cd985f8ff9afdcba0cc67d6a4
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-06-19 17:07:16 +0300

    teht. 8.4

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     authorCount: Int!
     bookCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -131,7 +131,12 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books;
+      }
+      return books.filter((b) => b.author === args.author);
+    },
     allAuthors: () => authors,
   },
 };
