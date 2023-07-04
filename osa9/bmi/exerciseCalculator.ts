4bc9270d9704fc72da0c01648a84d909722991a5
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-07-04 15:44:24 +0300

    teht. 9.2

---
+++ osa9/bmi/exerciseCalculator.ts
@@ -0,0 +1,56 @@
+interface Rating {
+  rating: number;
+  ratingDescription: string;
+}
+
+interface CalculatorResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const rate = (average: number, target: number): Rating => {
+  if (average < target / 2) {
+    return {
+      rating: 1,
+      ratingDescription: 'goal was not met, you should try a bit harder',
+    };
+  } else if (average < target) {
+    return {
+      rating: 2,
+      ratingDescription: 'not too bad but could be better',
+    };
+  } else {
+    return {
+      rating: 3,
+      ratingDescription: 'goal was met, you can congratulate yourself',
+    };
+  }
+};
+
+const calculateExercises = (
+  hours: number[],
+  target: number
+): CalculatorResult => {
+  const periodLength = hours.length;
+  const trainingDays = hours.filter((h) => h > 0).length;
+  const average = hours.reduce((a, b) => a + b, 0) / periodLength;
+  const success = average >= target;
+  const { rating, ratingDescription } = rate(average, target);
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
