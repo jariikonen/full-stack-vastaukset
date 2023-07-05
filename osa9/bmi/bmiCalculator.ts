7aa03a068f2da9f683a18b9f783222d4d252a0d4
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-07-05 09:36:08 +0300

    teht. 9.5

--- osa9/bmi/bmiCalculator.ts
+++ osa9/bmi/bmiCalculator.ts
@@ -1,23 +1,4 @@
-interface BmiValues {
-  height: number;
-  weight: number;
-}
-
-const parseBmiArguments = (args: string[]): BmiValues => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
-
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3]),
-    };
-  } else {
-    throw new Error('Provided values were not numbers!');
-  }
-};
-
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / (height / 100) ** 2;
 
   if (bmi < 16.0) {
@@ -38,14 +19,3 @@ const calculateBmi = (height: number, weight: number): string => {
     return 'Obese (Class III)';
   }
 };
-
-try {
-  const { height, weight } = parseBmiArguments(process.argv);
-  console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-  let errorMessage = 'Something bad happened.';
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
-  }
-  console.log(errorMessage);
-}
--- osa9/bmi/index.ts
+++ osa9/bmi/index.ts
@@ -1,11 +1,26 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query;
+  if (!isNaN(Number(height)) && !isNaN(Number(weight))) {
+    const bmi = calculateBmi(Number(height), Number(weight));
+    res.json({
+      heigth: Number(height),
+      weight: Number(weight),
+      bmi,
+    });
+  } else {
+    res.status(400).send({ error: 'malformatted parameters' });
+  }
+});
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
