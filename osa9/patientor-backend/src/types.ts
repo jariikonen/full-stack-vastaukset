074dc4e9df324eccd46d9b82b3d0f03a1846f7c5
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-07-15 13:43:40 +0300

    teht. 9.22

--- osa9/patientor-backend/src/types.ts
+++ osa9/patientor-backend/src/types.ts
@@ -4,14 +4,75 @@ export interface DiagnosisEntry {
   latin?: string;
 }
 
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnosisEntry["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+export type NewBaseEntry = Omit<BaseEntry, "id">;
+
+export type NewHealthCheckEntry = Omit<HealthCheckEntry, "id">;
+
+export type NewOccupationalHealthcareEntry = Omit<
+  OccupationalHealthcareEntry,
+  "id"
+>;
+
+export type NewHospitalEntry = Omit<HospitalEntry, "id">;
+
+// Define special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown
+  ? Omit<T, K>
+  : never;
+
+export type NewEntry = UnionOmit<Entry, "id">;
+
 export enum Gender {
   Female = "female",
   Male = "male",
   Other = "other",
 }
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
 
 export interface PatientEntry {
   id: string;
--- osa9/patientor-backend/src/utils.ts
+++ osa9/patientor-backend/src/utils.ts
@@ -1,4 +1,17 @@
-import { NewPatientEntry, Gender, Entry } from "./types";
+import {
+  NewPatientEntry,
+  Gender,
+  NewEntry,
+  Entry,
+  NewHealthCheckEntry,
+  HealthCheckRating,
+  NewBaseEntry,
+  DiagnosisEntry,
+  NewOccupationalHealthcareEntry,
+  NewHospitalEntry,
+  SickLeave,
+  Discharge,
+} from "./types";
 
 const isString = (text: unknown): text is string => {
   return typeof text === "string" || text instanceof String;
@@ -38,6 +51,121 @@ const parseGender = (gender: unknown): Gender => {
   return gender;
 };
 
+const isEntryType = (
+  entryType: unknown
+): entryType is "HealthCheck" | "OccupationalHealthcare" | "Hospital" => {
+  if (
+    !isString(entryType) ||
+    (entryType !== "HealthCheck" &&
+      entryType !== "OccupationalHealthcare" &&
+      entryType !== "Hospital")
+  ) {
+    throw new Error(`Incorrect entry type ${JSON.stringify(entryType)}.`);
+  }
+  return true;
+};
+
+const toNewBaseEntry = (object: object): NewBaseEntry => {
+  const baseObject = object;
+
+  if (
+    !(
+      "description" in baseObject &&
+      "date" in baseObject &&
+      "specialist" in baseObject
+    )
+  ) {
+    throw new Error("Incorrect base-entry: missing field");
+  }
+
+  const newBaseEntry: NewBaseEntry = {
+    description: baseObject.description as string,
+    date: baseObject.date as string,
+    specialist: baseObject.specialist as string,
+  };
+
+  if ("diagnosisCodes" in object) {
+    newBaseEntry.diagnosisCodes = object.diagnosisCodes as Array<
+      DiagnosisEntry["code"]
+    >; // EI TARKISTETA!
+  }
+
+  return newBaseEntry;
+};
+
+const toNewHealthCheckEntry = (object: object): NewHealthCheckEntry => {
+  const newEntry = toNewBaseEntry(object) as NewHealthCheckEntry;
+  if (!("healthCheckRating" in object)) {
+    throw new Error("Incorrect health-check-entry");
+  }
+
+  newEntry.type = "HealthCheck";
+  newEntry.healthCheckRating = object.healthCheckRating as HealthCheckRating; // EI TARKISTETA!
+  return newEntry;
+};
+
+const toNewOccupationalHealthcareEntry = (
+  object: object
+): NewOccupationalHealthcareEntry => {
+  const newEntry = toNewBaseEntry(object) as NewOccupationalHealthcareEntry;
+  if (!("employerName" in object)) {
+    throw new Error("Incorrect occupational-healthcare-entry");
+  }
+
+  newEntry.type = "OccupationalHealthcare";
+  newEntry.employerName = object.employerName as string; // EI TARKISTETA!
+
+  if ("sickLeave" in object) {
+    newEntry.sickLeave = object.sickLeave as SickLeave; // EI TARKISTETA!
+  }
+
+  return newEntry;
+};
+
+const toNewHospitalEntry = (object: object): NewHospitalEntry => {
+  const newEntry = toNewBaseEntry(object) as NewHospitalEntry;
+  if (!("discharge" in object)) {
+    throw new Error("Incorrect hospital-entry");
+  }
+
+  newEntry.type = "Hospital";
+  newEntry.discharge = object.discharge as Discharge; // EI TARKISTETA!
+
+  return newEntry;
+};
+
+// Helper function for exhaustive type checking
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+const toNewEntry = (object: unknown): NewEntry => {
+  if (!object || typeof object !== "object") {
+    throw new Error("Missing or incorrect entry.");
+  }
+
+  if ("type" in object) {
+    if (!isEntryType(object.type)) {
+      throw new Error("Incorrect entry type");
+    }
+
+    switch (object.type) {
+      case "HealthCheck":
+        return toNewHealthCheckEntry(object);
+      case "OccupationalHealthcare":
+        return toNewOccupationalHealthcareEntry(object);
+      case "Hospital":
+        return toNewHospitalEntry(object);
+      default:
+        return assertNever(object.type);
+    }
+  }
+
+  throw new Error("Incorrect data: a field is missing.");
+};
+
 const toNewPatientEntry = (object: unknown): NewPatientEntry => {
   if (!object || typeof object !== "object") {
     throw new Error("Incorrect or missing data.");
@@ -51,13 +179,26 @@ const toNewPatientEntry = (object: unknown): NewPatientEntry => {
     "occupation" in object &&
     "entries" in object
   ) {
+    if (!Array.isArray(object.entries)) {
+      throw new Error("Incorrect entries: not an array");
+    }
+
+    const entries: Entry[] = object.entries.map((e: unknown) => {
+      const entry = toNewEntry(e) as Entry;
+      if (!e || typeof e !== "object" || !("id" in e)) {
+        throw new Error("Incorrect entry: missing id");
+      }
+      entry.id = e.id as string;
+      return entry;
+    });
+
     const newEntry: NewPatientEntry = {
       name: parseStringField(object.name, "name"),
       dateOfBirth: parseDate(object.dateOfBirth),
       ssn: parseStringField(object.ssn, "ssn"),
       gender: parseGender(object.gender),
       occupation: parseStringField(object.occupation, "occupation"),
-      entries: object.entries as Entry[],
+      entries: entries,
     };
 
     return newEntry;
